bae261ef671128a0c8458d37ea0a1e5402a8aad4
Dihuu <62663759+mahirlabibdihan@users.noreply.github.com>
2020-09-14 14:56:06 +0600

    Update decima_to_binary.c

--- decima_to_binary.c
+++ decima_to_binary.c
@@ -1,7 +1,20 @@
-int decbin(int n) 
-{ 
-    if(n) return n%2+10*decbin(n/2);
+#include<stdio.h>
+int decbin(int n)
+{
+	if(n) return n%2+10*decbin(n/2);
 }
+
+int dec_bin(int n) 
+{ 
+    int i,s=0;
+    
+    for(i=1;n;i*=10)
+    {
+       s+=(n%2)*i;
+       n/=2;
+    }
+    return s; 
+} 
 
 int main()
 {
