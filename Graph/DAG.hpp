0efdd56645dd98e3782daf51bfaa1e0fa5a7a3f1
mahirlabibdihan <mahirlabibdihan@gmail.com>
2022-05-21 22:15:14 +0600

    dag

--- Graph/DAG.hpp
+++ Graph/DAG.hpp
@@ -1,37 +1,28 @@
 #include <vector>
 using namespace std;
 // This function is a variation of DFSUtil() in https://www.geeksforgeeks.org/archives/18212
-bool checkCycle(int u, vector<int> adj[], vector<bool> &visited, vector<bool> &recStack)
+bool checkCycle(int u, vector<int> adj[], vector<bool> &visited, vector<bool> recStack)
 {
-    if (visited[u] == false)
+    visited[u] = true;
+    recStack[u] = true;
+    for (auto v : adj[u])
     {
-        visited[u] = true;
-        recStack[u] = true;
-        for (auto v : adj[u])
+        if (!visited[v] && checkCycle(v, adj, visited, recStack))
         {
-            if (!visited[v] && checkCycle(v, adj, visited, recStack))
-            {
-                return true;
-            }
-            else if (recStack[v])
-            {
-                return true;
-            }
+            return true;
+        }
+        else if (recStack[v])
+        {
+            return true;
         }
     }
-    recStack[u] = false; // remove the vertex from recursion stack
     return false;
 }
 
 bool isDAG(vector<int> adj[], int n)
 {
-    vector<bool> visited(n);
-    vector<bool> recStack(n);
-    for (int i = 0; i < n; i++)
-    {
-        visited[i] = false;
-        recStack[i] = false;
-    }
+    vector<bool> visited(n, false);
+    vector<bool> recStack(n, false);
     for (int i = 0; i < n; i++)
     {
         if (!visited[i] && checkCycle(i, adj, visited, recStack))
